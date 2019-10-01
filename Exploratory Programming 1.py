9ae99b160e7fb048b573a29a62300d5e292a08c6
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-10-01 09:54:13 -0400

    Update 4
    Added the print command so that the code actually runs.

--- Exploratory Programming 1.py
+++ Exploratory Programming 1.py
@@ -14,7 +14,7 @@ def double(sequence):
 # In[4]:
 
 #attempt to multiply variable by two
-double([4])
+print double([4])
 
 
 # In[5]:
@@ -30,19 +30,19 @@ def quadruple(sequence):
 # In[6]:
 
 #first attempt to multiply listed numbers by four
-quadruple([1,3,5,7,9])
+print quadruple([1,3,5,7,9])
 
 
 # In[7]:
 
 #second attempt to multiply list by four
-quadruple([2,4,6,8,10])
+print quadruple([2,4,6,8,10])
 
 
 # In[10]:
 
 #attempt to multiply by four, with non integer variable
-quadruple([100,200,300,'you guys are gonna make me rich! '])
+print quadruple([100,200,300,'you guys are gonna make me rich! '])
 # Notably, I do not know how to run things in Atom, but when called, the sequence does run in Jupyter.
 
 # In[ ]:
